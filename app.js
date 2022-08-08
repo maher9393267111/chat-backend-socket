9ae84dfe750855b9b7b2c0e3821173a5ec4ed728
maher <gomemahero.com>
2022-08-09 02:15:06 +0300

    work good

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 
+// const express = require('express');
+// const app = express();
+// const userRoutes = require('./routes/userRoutes')
+// const User = require('./models/User');
+// //const Message = require('./models/Message')
+// const rooms = ['general', 'tech', 'finance', 'crypto'];
+// const cors = require('cors');
+
+// console.log('starting server');
+
+
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+// app.use(cors());
+
+// app.use('/users', userRoutes)
+// require('./connection')
+
+// const server = require('http').createServer(app);
+// const PORT = 5001;
+// const io = require('socket.io')(server, {
+//   cors: {
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST']
+//   }
+// })
+
+
+// server.listen(PORT, ()=> {
+//     console.log('listening to port', PORT)
+//   })
+
+
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes')
 const User = require('./models/User');
-//const Message = require('./models/Message')
+const Message = require('./models/Message')
 const rooms = ['general', 'tech', 'finance', 'crypto'];
 const cors = require('cors');
-
-console.log('starting server');
-
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -27,6 +57,83 @@ const io = require('socket.io')(server, {
 })
 
 
-server.listen(PORT, ()=> {
-    console.log('listening to port', PORT)
+async function getLastMessagesFromRoom(room){
+  let roomMessages = await Message.aggregate([
+    // find messages in room where to = room
+    {$match: {to: room}},
+    // then  change  form  of data with group
+    {$group: {_id: '$date', messagesByDate: {$push: '$$ROOT'}}}
+  ])
+  console.log('💠💠💠💠💠💠',roomMessages)
+  return roomMessages;
+}
+
+function sortRoomMessagesByDate(messages){
+  return messages.sort(function(a, b){
+    let date1 = a._id.split('/');
+    let date2 = b._id.split('/');
+
+    date1 = date1[2] + date1[0] + date1[1]
+    date2 =  date2[2] + date2[0] + date2[1];
+
+    return date1 < date2 ? -1 : 1
   })
+}
+
+// socket connection
+
+io.on('connection', (socket)=> {
+
+  socket.on('new-user', async ()=> {
+    const members = await User.find();
+    io.emit('new-user', members)
+  })
+
+  socket.on('join-room', async(newRoom, previousRoom)=> {
+    socket.join(newRoom);
+    socket.leave(previousRoom);
+    let roomMessages = await getLastMessagesFromRoom(newRoom);
+    roomMessages = sortRoomMessagesByDate(roomMessages);
+    socket.emit('room-messages', roomMessages)
+  })
+
+  socket.on('message-room', async(room, content, sender, time, date) => {
+    // save message to db when receiving message from client
+    const newMessage = await Message.create({content, from: sender, time, date, to: room});
+    // then fetch all room messages from db
+    let roomMessages = await getLastMessagesFromRoom(room);
+    // then sort them by date
+    roomMessages = sortRoomMessagesByDate(roomMessages);
+    // sending message to room
+    io.to(room).emit('room-messages', roomMessages);
+    socket.broadcast.emit('notifications', room)
+  })
+
+  app.delete('/logout', async(req, res)=> {
+    try {
+      const {_id, newMessages} = req.body;
+      const user = await User.findById(_id);
+      user.status = "offline";
+      user.newMessages = newMessages;
+      await user.save();
+      const members = await User.find();
+      // when user logs out, all rooms are left
+      socket.broadcast.emit('new-user', members);
+      res.status(200).send();
+    } catch (e) {
+      console.log(e);
+      res.status(400).send()
+    }
+  })
+
+})
+
+// send all rooms to client
+app.get('/rooms', (req, res)=> {
+  res.json(rooms)
+})
+
+
+server.listen(PORT, ()=> {
+  console.log('listening to port', PORT)
+})
